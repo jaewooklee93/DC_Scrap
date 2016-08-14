1ab45398136dfebfa75b420c8ef00b98ed3f9bb3
J. Lee <jaewooklee@snu.ac.kr>
2016-08-15 03:55:17 +0900

    v2

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re, os, requests, pickle, urllib
 from bs4 import BeautifulSoup
 from urlparse import urlparse
 from os.path import splitext, basename
+from selenium import webdriver # install selenium nodejs phantomjs 
 
 dir_name='collect' # 파일을 저장할 폴더의 이름
 gall_id='tree' # 식물 갤러리
@@ -17,25 +18,28 @@ done_list = [] # 한번 다운받은 페이지는 skip
 def init():
 	global done_list
 	try: 
-		done_list=pickle.load(open('done_list.log','r')) # 여러 번 실행하는 경우 한번만 scrap
+		done_list=pickle.load(open(dir_name+'/images/common/done_list.log','r')) # 여러 번 실행하는 경우 한번만 scrap
 	except:
 		pass
 	try:
 		os.makedirs(dir_name+'/images/common') # 처음 실행하는 경우, 폴더를 구성
 	except:
 		pass
+	global driver
+	driver=webdriver.PhantomJS()
 
 def cleanup():
-	pickle.dump(done_list,open('done_list.log','w'))
+	pickle.dump(done_list,open(dir_name+'/images/common/done_list.log','w'))
 
-def get_html(url):
-	return requests.get(url).text
 
-def save_page(url):
-	page_no=re.findall(r'\d+', url)[0] # 글 번호
+def save_page(href):
+	page_no=re.findall(r'\d+', href)[0] # 글 번호
 
-	html=get_html(url)
+	url=domain+href
+	driver.get(url)
+	html=driver.page_source
 	soup=BeautifulSoup(html,'lxml')
+
 
 	for link in soup.select('img'):
 		try:
@@ -44,23 +48,25 @@ def save_page(url):
 				img=urllib.urlopen(img_src.replace('dcimg1','dcimg2'))
 				img_name=img.info().getheader('Content-Disposition')[21:]
 				open(dir_name+'/images/'+img_name, 'wb').write(img.read())
-				html=html.replace(img_src,'images/'+img_name)
+
+				link['src']='images/'+img_name
 			elif img_src:
 				img_name=basename(urlparse(img_src).path)
 				if not os.path.isfile(dir_name+'/images/common/'+img_name):
 					img=urllib.urlopen(img_src)
 					open(dir_name+'/images/common/'+img_name, 'wb').write(img.read())
-				html=html.replace(img_src,'images/common/'+img_name)
+
+					link['src']='images/common/'+img_name
 		except:
-			print img_name
+			pass
 
 	filename=dir_name+'/['+str(page_no)+'] '+soup.title.string+'.html'
-	open(filename,'w').write(html.encode('utf8'))
+	open(filename,'w').write(soup.encode('utf8'))
 	print "page_no: "+page_no
 
 
 def scrap():
-	html=get_html(domain+recommend_url)
+	html=requests.get(domain+recommend_url).text
 	soup=BeautifulSoup(html,'lxml')
 
 	for link in soup.select('td > a'): # 개념글들의 url을 추출
@@ -68,7 +74,7 @@ def scrap():
 		if icon and icon[0] in ['icon_pic_b', 'icon_txt_b', 'sec_icon']:
 			href=link.get('href')
 			if not href in done_list:
-				save_page(domain+href)
+				save_page(href)
 				done_list.append(href)
 
 
